b807c085e12636b0e4e05051206037dd6a449a0f
PaulLiuEngineering <paulliu14@gmail.com>
2021-04-18 17:15:57 -0500

    search

--- app/static/script/modal_watched.js
+++ app/static/script/modal_watched.js
@@ -36,6 +36,9 @@ $(function() {
             type: 'POST',
             success: function(response) {
                 console.log(response);
+                document.open();
+                document.write(`${response}`);
+                document.close();
             },
             error: function(error) {
                 console.log(error);
